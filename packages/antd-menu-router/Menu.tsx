c6829fac19f6e9e7d0d86a3361b8f5a7f2ae1805
hao.li <hao.li@funplus.com>
2022-04-27 20:22:15 +0800

    refactor: antd-menu-router

--- packages/antd-menu-router/Menu.tsx
+++ packages/antd-menu-router/Menu.tsx
@@ -1,12 +1,14 @@
 import { Menu as AntdMenu } from 'antd'
 import { ItemType } from 'antd/lib/menu/hooks/useItems'
-import { MenuDividerType, MenuItemGroupType, SubMenuType } from 'rc-menu/lib/interface'
-import { FunctionComponent, useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
 import { MenuInfo } from 'rc-menu/es/interface'
+import { MenuDividerType, MenuItemGroupType, SubMenuType } from 'rc-menu/lib/interface'
+import { FunctionComponent } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-export type MenuItem = Omit<Exclude<NonNullable<ItemType>, MenuItemGroupType | MenuDividerType>, 'children'> &
-  (
+export type MenuItem = Omit<Exclude<NonNullable<ItemType>, MenuItemGroupType | MenuDividerType>, 'children' | 'key'> & {
+  key: string
+  keys: string[] // 所有祖先节点的 key 组成的数组，由浅入深排列。
+} & (
     | {
         path: string
         children?: never
@@ -17,12 +19,12 @@ export type MenuItem = Omit<Exclude<NonNullable<ItemType>, MenuItemGroupType | M
       }
   )
 
-function flattenItems(items: MenuItem[], flattenedItems: MenuItem[] = []) {
+function flattenItems(items: MenuItem[], flattenedItems: MenuItem[] = [], keys: string[] = []) {
   for (const item of items) {
     if ((item as SubMenuType).children) {
-      flattenItems((item as SubMenuType).children as MenuItem[], flattenedItems)
+      flattenItems((item as SubMenuType).children as MenuItem[], flattenedItems, [...keys, item.key])
     } else {
-      flattenedItems.push(item)
+      flattenedItems.push(Object.assign(item, { keys: keys }))
     }
   }
   return flattenedItems
@@ -32,7 +34,7 @@ function useActivatedMenu(flattenedItems: MenuItem[]) {
   const location = useLocation()
 
   if (location.pathname) {
-    return flattenedItems.find(item => location.pathname === item.key + '') || null
+    return flattenedItems.find(item => location.pathname === item.path + '') || null
   }
 
   return null
@@ -44,29 +46,31 @@ export interface MenuProps {
 
 const Menu: FunctionComponent<MenuProps> = props => {
   const { items } = props
-  const [selectedKeys, setSelectedKeys] = useState<string[]>()
-  const [flattenedItems, setFlattenedItems] = useState<MenuItem[]>([])
-  const activatedItem = useActivatedMenu(flattenedItems)
   const navigate = useNavigate()
+  const flattenedItems = flattenItems(items)
+  const activatedItem = useActivatedMenu(flattenedItems)
+  const defaultItem = activatedItem || flattenedItems[0] || null
+  const defaultSelectedKeys = defaultItem ? [defaultItem.key] : []
+  const defaultOpenKeys = defaultItem ? defaultItem.keys : []
 
-  useEffect(() => {
-    setFlattenedItems(flattenItems(items))
-  }, [items])
-
-  useEffect(() => {
-    setSelectedKeys(activatedItem?.key ? [activatedItem.key + ''] : [])
-  }, [activatedItem?.key])
-
-  const onClick = ({ key }: MenuInfo) => {
-    const item = flattenedItems.find(item => key === item.key)
+  const onClick = (info: MenuInfo) => {
+    const item = flattenedItems.find(menuItem => info.key === menuItem.key)
 
     if (item && item.path) {
       navigate(item.path)
-      setSelectedKeys([key])
     }
   }
 
-  return <AntdMenu selectedKeys={selectedKeys} items={items} theme="dark" onClick={onClick} />
+  return (
+    <AntdMenu
+      defaultOpenKeys={defaultOpenKeys}
+      defaultSelectedKeys={defaultSelectedKeys}
+      items={items}
+      theme="dark"
+      mode="inline"
+      onClick={onClick}
+    />
+  )
 }
 
 Menu.defaultProps = {
